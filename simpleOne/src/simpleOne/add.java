b9cf72493eea8aa9048d97d129947473da1d218b
Divyabd <divyagowda8393@gmail.com>
2021-02-04 17:26:17 +0530

    conflict find

--- simpleOne/src/simpleOne/add.java
+++ simpleOne/src/simpleOne/add.java
@@ -4,9 +4,9 @@ public class add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=1;
-int b=2;
-System.out.println(a+b);
+int m=1;
+int n=2;
+System.out.println(m+n);
 	}
 
 }
