b82209a98925b1419feaaa31f563e63a4ab7c8c4
Divyabd <divyagowda8393@gmail.com>
2021-02-04 17:34:09 +0530

    adding m

--- simpleOne/src/simpleOne/add.java
+++ simpleOne/src/simpleOne/add.java
@@ -5,17 +5,19 @@ public class add {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-int m=1;
-int n=2;
-System.out.println(m+n);
+		int m = 1;
+		int n = 2;
+		System.out.println(m + n);
 
-int a=1;
-int b=2;
-System.out.println(a+b);
-		int c=1;
-int d=2;
-System.out.println(c+d);
+		int a = 1;
+		int b = 2;
+		System.out.println(a + b);
+		int c = 1;
+		int d = 2;
+		System.out.println(c + d);
 
+		int s=2;
+		System.out.println(s);
 	}
 
 }
