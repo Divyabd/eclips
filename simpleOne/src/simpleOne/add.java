80a50f878a12746d58807105ebfeeb9e0c02792d
Divyabd <divyagowda8393@gmail.com>
2021-02-05 08:03:25 +0530

    deleted feature

--- simpleOne/src/simpleOne/add.java
+++ simpleOne/src/simpleOne/add.java
@@ -8,10 +8,6 @@ public class add {
 		int m = 1;
 		int n = 2;
 		System.out.println(m + n);
-
-		int a = 1;
-		int b = 2;
-		System.out.println(a + b);
 		int c = 1;
 		int d = 2;
 		System.out.println(c + d);
